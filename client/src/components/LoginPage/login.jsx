04928f8a9a3d2876fc4c95ace85415d571355b15
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-04-18 21:04:11 -0700

    update login.jsx to import css

--- client/src/components/LoginPage/login.jsx
+++ client/src/components/LoginPage/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './login.css'
+import './login.css';
 
 
 
@@ -37,4 +37,4 @@ const LoginPage = () =>  {
     )
 }
 
-export default LoginPage
+export default LoginPage;
