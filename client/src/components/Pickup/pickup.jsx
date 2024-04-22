6cd921fc4c8fc2cea5f62db2c5063d14e213481b
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-04-22 15:36:15 -0700

    edit styling for date/time picker under request pickup

--- client/src/components/Pickup/pickup.jsx
+++ client/src/components/Pickup/pickup.jsx
@@ -1,66 +1,66 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import './TimePicker.css';
-import './pickup.css'
-import TimePicker from 'react-time-picker';
- import 'react-clock/dist/Clock.css';
- import "react-datepicker/dist/react-datepicker.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "./TimePicker.css";
+// import './pickup.css'
+import TimePicker from "react-time-picker";
+import "react-clock/dist/Clock.css";
+import "react-datepicker/dist/react-datepicker.css";
 // CSS Modules, react-datepicker-cssmodules.css
- import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
-
+import "react-datepicker/dist/react-datepicker-cssmodules.css";
 
 const Pickup = () => {
   const [startDate, setStartDate] = useState(new Date());
-  const [time, setTime] = useState('10:00');
+  const [time, setTime] = useState("10:00");
 
   const [formData, setFormData] = useState({
-    Date: '',
-    Time: '',
-    Location: '',
-    Notes: ''
+    Date: "",
+    Time: "",
+    Location: "",
+    Notes: "",
   });
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    const location = document.getElementById('location').value;
-    const notes = document.getElementById('notes').value;
+    const location = document.getElementById("location").value;
+    const notes = document.getElementById("notes").value;
 
     setFormData({
       Date: startDate,
       Time: time,
       Location: location,
-      Notes: notes
+      Notes: notes,
     });
 
     console.log(formData);
 
     // send the request/mutation
-
   }
 
   return (
     <div className="container">
+      <h2>Request Pickup Form</h2>
 
       <form onSubmit={handleSubmit}>
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-
+        <label>Pick a date</label>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+        <label>Enter a time</label>
         <TimePicker onChange={setTime} value={time} />
 
-        <label>Location: </label>
-        <input type="text" name="Location" id="location"/>
+        <label className="addressLbl">Enter address location:</label>
+        <input type="text" name="Location" id="location" />
 
         <label> Notes: </label>
         <textarea id="notes"> </textarea>
 
-        <button type="submit">Submit</button>
+        <button className="submit" type="submit">Submit</button>
+
       </form>
-
     </div>
-
   );
-}
+};
 
 export default Pickup;
-
