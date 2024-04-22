85f8e4d8ce530e163c0e59aa0eb9eb39bbd650cf
Amanda Corey <76081915+Amjcorey@users.noreply.github.com>
2024-04-22 13:40:56 -0700

    work on styling for login form and adjust styling code in index.css

--- client/src/components/LoginPage/login.jsx
+++ client/src/components/LoginPage/login.jsx
@@ -1,40 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './login.css';
+import "./login.css";
 
+const LoginPage = () => {
+  return (
+    <div className="loginWrapper">
+      <h2>Client Login</h2>
+      <form>
+        <div className="container">
+          <label className="username">Username:</label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            required
+          />
 
+          <label className="password">Password:</label>
+          <input
+            type="text"
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
 
-
-const LoginPage = () =>  {
-    return (
-        <div className='loginBody'>
-            {/* <img></img> */}
-            <h2>
-                Authentication Required
-            </h2>
-            <form>
-             <p>
-                <label>
-                    Username:
-                </label>
-                <input type='text' name='username' required />
-            </p>
-            <p>
-            <label>
-                    Password:
-                </label>
-                <input type='password' name='password' required /> 
-            </p>
-            <p>
-                <button id='login_button' type='submit'>
-                    Login
-                </button>
-            </p>   
-            </form>
-            
+          <button className="loginBtn" type="submit">
+            Login
+          </button>
+          {/* reveiw https://react.tips/checkboxes-in-react-16/
+           <label>
+            <input type="checkbox" checked="checked" name="remember" />
+            Remember me
+          </label> */}
         </div>
-    )
-}
+        <div className="container">
+          <button type="button" className="cancelbtn">
+            Cancel
+          </button>
+          <span className="password">
+            Forgot <a href="#">password?</a>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
